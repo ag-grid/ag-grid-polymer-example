a9065c32e59eee5495153cb3db9793201f25fd06
Sean Landsman <sean@thelandsmans.com>
2019-10-24 13:06:11 +0100

    AG-1329 Move ag-grid-polymer to modules, delete legacy ag-grid-angular, ag-grid-react, ag-grid-vue. Various fixes

--- example/ag-grid-polymer-example.js
+++ example/ag-grid-polymer-example.js
@@ -1,5 +1,5 @@
 import {html, PolymerElement} from '@polymer/polymer/polymer-element.js';
-import 'ag-grid-polymer';
+import '@ag-community/grid-polymer';
 import "@ag-community/client-side-row-model";
 
 import ClickableCellRenderer from './clickable-renderer'
