e6b76c56fc13492014d661855bf6ed42b0867d3d
seanlandsman <sean@thelandsmans.com>
2022-02-25 11:02:12 +0000

    AG-6297 [Regression] When using Polymer and unchecking or checking an item in the filter pop-out, the console logs an error and the grid is not filtered due to missing unwrap() method in polymer PolymerFrameworkComponentWrapper code

--- example/ag-grid-polymer-example.js
+++ example/ag-grid-polymer-example.js
@@ -6,9 +6,9 @@ import ClickableCellRenderer from './clickable-renderer'
 class AgGridPolymerExample extends PolymerElement {
     static get template() {
         return html`
-            <link rel="stylesheet" href="../node_modules/@ag-grid-community/all-modules/dist/styles/ag-grid.css">
-            <link rel="stylesheet" href="../node_modules/@ag-grid-community/all-modules/dist/styles/ag-theme-alpine.css">
-          
+            <link rel="stylesheet" href="../node_modules/@ag-grid-enterprise/all-modules/dist/styles/ag-grid.css">
+            <link rel="stylesheet" href="../node_modules/@ag-grid-enterprise/all-modules/dist/styles/ag-theme-alpine.css">
+
             <div style="position: absolute; top: 0; left: 0; width: 100%; height: 100%; overflow: hidden; padding: 10px;">
                 <h1>Simple AG Grid Polymer 3 Example</h1>
                 <ag-grid-polymer style="width: 800px; height: 250px;"
@@ -16,19 +16,35 @@ class AgGridPolymerExample extends PolymerElement {
                                  rowData="{{rowData}}"
                                  columnDefs="{{columnDefs}}"
                                  components="{{components}}"
+                                 defaultColDef="{{defaultColDef}}"
                                  on-first-data-rendered="{{firstDataRendered}}"
-                                 ></ag-grid-polymer>
+                ></ag-grid-polymer>
             </div>
-    `;
+        `;
     }
 
     constructor() {
         super();
 
+        this.defaultColDef = {
+            filter: true,
+            floatingFilter: true
+        };
+
         this.columnDefs = [
-            {headerName: "Make", field: "make"},
-            {headerName: "Model", field: "model"},
-            {headerName: "Price", field: "price"},
+            {field: "make", filter: true},
+            {field: "model", filter: 'agTextColumnFilter'},
+            {
+                field: "price",
+                filter: 'agNumberColumnFilter',
+                filterParams: {
+                    allowedCharPattern: '\\d',
+                    numberParser: (text) => {
+                        if (!Number(text)) return null;
+                        return parseInt(text);
+                    }
+                }
+            },
             {
                 headerName: "Clickable Component",
                 field: "make",
